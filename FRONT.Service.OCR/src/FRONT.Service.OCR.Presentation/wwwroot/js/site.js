5e0fefba1850627b70b9126da194265113912838
Tiago Pereira Cardoso <tiagocardosso@gmail.com>
2024-11-11 09:49:20 +0100

    change layout poc

--- FRONT.Service.OCR/src/FRONT.Service.OCR.Presentation/wwwroot/js/site.js
+++ FRONT.Service.OCR/src/FRONT.Service.OCR.Presentation/wwwroot/js/site.js
@@ -1,5 +1,4 @@
 ﻿$(document).ready(function () {
-
     var inputFile = document.getElementById("tm-choose-file");
     var inputFileName = document.getElementById("tm-file-name");
     inputFile.addEventListener("change", tmScript);
@@ -7,7 +6,6 @@
     function tmScript() {
         inputFileName.innerText = this.files[0].name;
     }
-
 
     jQuery(".input-file-wrap label").append("<span class='file-name'> NO FILE CHOOSEN </span>");
     jQuery('.input-file-wrap input[type="file"]').change(function (e) {
@@ -23,48 +21,39 @@
         $(".loading").show();
         setTimeout(function () {
             var file = $("#tm-choose-file")[0].files[0];
-            var formData = new FormData();
-            formData.append("file", file);
+            if ($("#tm-choose-file")[0].files.length > 0) {
+                var formData = new FormData();
+                formData.append("text", $("#questionChatGPT").val());
+                formData.append("file", file);
 
-            $.ajax({
-                url: "", // URL to send the file for OCR
-                type: "POST",
-                data: formData,
-                processData: false,
-                contentType: false,
-                error: function (a, b, c) {
-                    $(".loading").hide();
-                },
-                success: function (response) {
-                    $(".div-text").html(response.text);
+                $.ajax({
+                    url: "https://api.ocr.pro.dev.isoftforge.com/v1/process/ocr/chatgpt", // URL to send the file for OCR
+                    type: "POST",
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    error: function (a, b, c) {
+                        console.log(a);
+                        console.log(b);
+                        console.log(c);
 
-                    if ($("#questionChatGPT").val() != "") {
-                        var Json = new Object();
-                        Json.Question = $("#questionChatGPT").val() + response.text;
+                        $(".div-result").show();
+                        $(".div-text").html(a.responseJSON.message);
 
-                        JAjaxSync(
-                            "POST",
-                            "", // URL to send the question to ChatGPT
-                            JSON.stringify(Json),
-                            ReturnChatGPTError,
-                            null,
-                            ReturnChatGPTSuccess,
-                            null);
+                        $(".loading").hide();
+                    },
+                    success: function (response) {
+                        console.log(response);
+                        $(".div-text").html(response.text);
+
+                        $(".div-result").show();
+                        $(".loading").hide();
                     }
-                }
-            });
+                });
+            }
+            else {
+                $(".loading").hide();
+            }
         }, 1000);
     });
 });
-
-function ReturnChatGPTError(er, x, z) {
-    console.log(er);
-    console.log(x);
-    console.log(z);
-}
-
-function ReturnChatGPTSuccess(data) {
-    $(".div-text-final").html(data);
-    $(".loading").hide();
-    $(".div-result").show();
-}
